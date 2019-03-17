5686dc380988679b58ce6c58b3ca414ed71086c7
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-17 18:55:47 -0400

    part a should be completed

--- iss.py
+++ iss.py
@@ -3,35 +3,22 @@
 __author__ = "mhoelzer"
 
 
-import argparse
-import sys
 import requests
 
 
 def get_astronauts():
     url = "http://api.open-notify.org/astros.json"
     request = requests.get(url)
-    return request.json()
-
-
-def create_parser():
-    parser = argparse.ArgumentParser(
-        description="Perform transformation on input text.")
-    parser.add_argument(
-        "-a", "--astronauts", help="search for an astronaut")
-    return parser
+    req_data = request.json()
+    print("There are currently {} astronauts in space.".format(
+        req_data["number"]))
+    for person in req_data["people"]:
+        print("{} is currently aboard the {}.".format(
+            person["name"], person["craft"]))
 
 
 def main():
-    parser = create_parser()
-    namespace = parser.parse_args()
-    if not namespace:
-        parser.print_usage()
-        sys.exit(1)
-    if namespace.astronauts:
-        # print("{} is currently aboard the {}.".format(name, craft))
-        # print("There are currently {} astronauts in space.".format(number))
-        pass
+    get_astronauts()
 
 
 if __name__ == '__main__':
