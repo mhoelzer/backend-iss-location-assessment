c2b92e6440970d5a08df3760d126bd00d994701f
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-17 16:26:54 -0400

    some code, but changing to remove the parser nfo

--- iss.py
+++ iss.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = "mhoelzer"
+
+
+import argparse
+import sys
+import requests
+
+
+def get_astronauts():
+    url = "http://api.open-notify.org/astros.json"
+    request = requests.get(url)
+    return request.json()
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument(
+        "-a", "--astronauts", help="search for an astronaut")
+    return parser
 
 
 def main():
-    pass
+    parser = create_parser()
+    namespace = parser.parse_args()
+    if not namespace:
+        parser.print_usage()
+        sys.exit(1)
+    if namespace.astronauts:
+        # print("{} is currently aboard the {}.".format(name, craft))
+        # print("There are currently {} astronauts in space.".format(number))
+        pass
 
 
 if __name__ == '__main__':
